5b37a3b4beffdc763487b9b7d470be9635249a94
BASSI Yassine <yassinebassii@gmail.com>
2026-03-12 19:03:44 +0000

    enable cors from everywhere

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -3,7 +3,11 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({ 
+    origin: '*',
+    methods: 'GET,HEAD,PUT,POST,OPTIONS',
+    allowedHeaders: '*',
+   });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Gateway service running on port ${port}`);
